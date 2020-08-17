7e4716db95e1d0e43f0689dd51d45074f10fb368
Carl <carl@reststop.com>
2020-08-16 18:35:12 -0700

    Initial Commit

---
+++ CatFacts/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  CatFacts
+//
+//  Created by carl on 8/16/20.
+//  Copyright © 2020 Catalyst Art. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
